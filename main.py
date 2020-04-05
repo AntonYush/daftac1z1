7e96f450008104b8cb64871ecc19e13359d901b1
AntonYush <62251719+AntonYush@users.noreply.github.com>
2020-04-05 17:36:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,23 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+
+@app.get("/method")
+def get_method():
+    return {"message": "GET"}
+
+
+@app.post("/method")
+def post_method():
+    return {"message": "POST"}
+
+
+@app.put("/method")
+def put_method():
+    return {"message": "PUT"}
+
+
+@app.delete("/method")
+def delete_method():
+    return {"message": "DELETE"}
