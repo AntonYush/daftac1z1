f40fe67363894fdbe309933949d9b140205f2fb7
AntonYush <62251719+AntonYush@users.noreply.github.com>
2020-04-21 03:57:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request, Cookie
 from fastapi.responses import RedirectResponse, JSONResponse
+from hashlib import sha256
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -27,6 +28,38 @@ def welcome_page():
     return {"message": "Welcome there!"}
 
 
+@app.api_route(path="/method", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+def method_check(request: Request):
+    return {"method": request.method}
+
+
+@app.post("/patient", response_model=PatientPostResp)
+def patient_post(rq: PatientPostRq):
+    response = PatientPostResp(id=app.counter, patient=rq.dict())
+    patients[app.counter] = rq.dict()
+    app.counter += 1
+    return response
+
+
+@app.get("/patient/{patient_id}")
+def patient_get(patient_id):
+    patient_id = int(patient_id)
+    if patient_id in patients.keys():
+        return patients[patient_id]
+    raise HTTPException(status_code=204)
+
+
+class LoginRq(BaseModel):
+    login: str
+    password: str
+
+
+@app.post("/login")
+def login(request: LoginRq):
+    response = RedirectResponse("/welcome", status_code=200)
+    cookie_v = sha256(bytes("{}:{}".format(request.dict()["login"], request.dict()["password"]), "utf-8")).hexdigest()
+    response.set_cookie(key="session_token", value=cookie_v)
+    return response
 @app.api_route(path="/method", methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 def method_check(request: Request):
     return {"method": request.method}
