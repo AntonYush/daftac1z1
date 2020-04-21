3171bf6fba8fd06db3371aafb6f09e0011f3b062
AntonYush <62251719+AntonYush@users.noreply.github.com>
2020-04-21 17:54:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 app.counter = 0
-app.sessions = []
+app.sessions = ["admin"]
 patients = dict()
 
 
@@ -26,12 +26,8 @@ def main_page():
 
 @app.get("/welcome")
 def welcome_page(request: Request):
-    print(request.cookies)
-    print(app.sessions)
     if request.cookies.get("session_token") not in app.sessions:
-        print("if there")
         raise HTTPException(status_code=401)
-    print("out if")
     return {"message": "Welcome there!"}
 
 
@@ -41,16 +37,19 @@ def method_check(request: Request):
 
 
 @app.post("/patient", response_model=PatientPostResp)
-def patient_post(rq: PatientPostRq):
-    response = PatientPostResp(id=app.counter, patient=rq.dict())
-    patients[app.counter] = rq.dict()
+def patient_post(request: Request, data: PatientPostRq):
+    if request.cookies.get("session_token") not in app.sessions:
+        raise HTTPException(status_code=401)
+    response = PatientPostResp(id=app.counter, patient=data.dict())
+    patients[app.counter] = data.dict()
     app.counter += 1
     return response
 
 
 @app.get("/patient/{patient_id}")
-def patient_get(patient_id):
-    patient_id = int(patient_id)
+def patient_get(request: Request, patient_id: int):
+    if request.cookies.get("session_token") not in app.sessions:
+        raise HTTPException(status_code=401)
     if patient_id in patients.keys():
         return patients[patient_id]
     raise HTTPException(status_code=204)
@@ -68,4 +67,14 @@ def login(request: Request):
     response.headers["Location"] = "/welcome"
     response.set_cookie(key="session_token", value=request.headers["Authorization"][5:])
     app.sessions.append(request.headers["Authorization"][5:])
+    return response
+
+
+@app.post("/logout")
+def logout(request: Request):
+    response = Response()
+    response.status_code = 303
+    response.headers["Location"] = "/"
+    app.sessions.pop(app.sessions.index(request.cookies.get("session_token")))
+    response.delete_cookie(key="session_token")
     return response
