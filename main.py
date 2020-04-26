306ca4c1163fe5d44ec076ab5f70caebe83760fd
AntonYush <62251719+AntonYush@users.noreply.github.com>
2020-04-26 13:11:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import  Response
+from fastapi.responses import Response
 from hashlib import sha256
 from pydantic import BaseModel
 
 app = FastAPI()
 app.counter = 0
-app.sessions = []
+app.users = ["dHJ1ZG5ZOlBhQzEzTnQ="]
 patients = dict()
 
 
@@ -26,7 +26,11 @@ def main_page():
 
 @app.get("/welcome")
 def welcome_page(request: Request):
-    if request.cookies.get("session_token") not in app.sessions:
+    if request.cookies.get("session_token") is None or \
+            request.cookies.get("session_token") not in app.users:
+        #print(request.cookies)
+        #print(request.cookies.get("session_token"))
+        #print(app.users[0])
         raise HTTPException(status_code=401)
     return {"message": "Welcome there!"}
 
@@ -38,7 +42,8 @@ def method_check(request: Request):
 
 @app.post("/patient", response_model=PatientPostResp)
 def patient_post(request: Request, data: PatientPostRq):
-    if request.cookies.get("session_token") not in app.sessions:
+    if request.cookies.get("session_token") is None or \
+            request.cookies.get("session_token") not in app.users:
         raise HTTPException(status_code=401)
     response = PatientPostResp(id=app.counter, patient=data.dict())
     patients[app.counter] = data.dict()
@@ -48,29 +53,34 @@ def patient_post(request: Request, data: PatientPostRq):
 
 @app.get("/patient/{patient_id}")
 def patient_get(request: Request, patient_id: int):
-    if request.cookies.get("session_token") not in app.sessions:
+    if request.cookies.get("session_token") is None or \
+            request.cookies.get("session_token") not in app.users:
         raise HTTPException(status_code=401)
     if patient_id in patients.keys():
         return patients[patient_id]
     raise HTTPException(status_code=204)
 
 
+class LoginRq(BaseModel):
+    login: str
+    password: str
+
+
 @app.post("/login")
 def login(request: Request):
-    print(request.headers["Authorization"]);
+    if request.headers.get("Authorization").split()[1] not in app.users:
+        return HTTPException(status_code=403)
     response = Response()
     response.status_code = 303
     response.headers["Location"] = "/welcome"
-    response.set_cookie(key="session_token", value=request.headers["Authorization"][5:])
-    app.sessions.append(request.headers["Authorization"][5:])
+    response.set_cookie(key="session_token", value=request.headers.get("Authorization").split()[1])
     return response
 
 
 @app.post("/logout")
-def logout(request: Request):
+def logout():
     response = Response()
     response.status_code = 303
     response.headers["Location"] = "/"
-    app.sessions.pop(app.sessions.index(request.cookies.get("session_token")))
     response.delete_cookie(key="session_token")
     return response
